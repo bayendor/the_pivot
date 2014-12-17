0de795705c79867b75777172ad03cec80ce3647a
David Bayendor <dbayendor@gmail.com>
2014-12-17 16:33:24 -0700

    Fix model validations / relationships

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,2 +1,13 @@
 class Tenant < ActiveRecord::Base
+  has_many :users
+  has_many :loan_requests, through: :users
+
+  validates :name, uniqueness: true
+  validates :slug, uniqueness: true
+
+  before_save :generate_slug
+
+  def generate_slug
+    self.slug = name.parameterize
+  end
 end
