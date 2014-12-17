40223639a075d3481fadfac5e3f44299ae5f2603
David Bayendor <dbayendor@gmail.com>
2014-12-17 15:00:02 -0700

    Configure router with tenant namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,44 @@
 Rails.application.routes.draw do
+  resources :tenants, path: '', param: :slug
 
-  root 'home#index'
+  namespace :tenants, as: :tenants, path: '/:slug' do
 
-  get '/admin', to: 'admin/base_admin#index', as: :admin_index
+    get '/admin', to: 'admin/base_admin#index', as: :admin_index
 
-  resources :users
-  resources :orders, only: [:index, :new, :show, :create]
+    resources :users
+    resources :orders, only: [:index, :new, :show, :create]
 
-  namespace :admin do
-    resources :items, :fillings, :users
-    resources :orders, only: [:index, :show, :update]
+    namespace :admin do
+      resources :items, :fillings, :users
+      resources :orders, only: [:index, :show, :update]
+    end
+
+    put 'admin/remove_item', to: 'admin/orders#remove_item'
+    post 'admin/update_quantity', to: 'admin/orders#update_quantity'
+
+    get    '/login',  to: 'sessions#new'
+    post   '/login',  to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+
+    get '/menu', to: 'menu#index'
+    resources :menu, only: [:show]
+
+    get  '/register', to: 'users#register'
+    post '/register', to: 'users#create'
+
+    get '/code', to: 'application#code'
+
+    get  '/build', to: 'line_items#index'
+    get '/build_item/:id', to: 'line_items#build_item'
+
+    post '/build_item', to: 'cart#create'
+    post  '/cart', to: 'cart#update_quantity'
+    delete '/cart', to: 'cart#remove_item'
+    get 'order/delivery', to: 'cart#delivery'
+
+    resources :cart, only: [:index]
   end
 
-  put 'admin/remove_item', to: 'admin/orders#remove_item'
-  post 'admin/update_quantity', to: 'admin/orders#update_quantity'
-
-  get    '/login',  to: 'sessions#new'
-  post   '/login',  to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-
-  get '/menu', to: 'menu#index'
-  resources :menu, only: [:show]
-
-  get  '/register', to: 'users#register'
-  post '/register', to: 'users#create'
-
-  get '/code', to: 'application#code'
-
-  get  '/build', to: 'line_items#index'
-  get '/build_item/:id', to: 'line_items#build_item'
-
-  post '/build_item', to: 'cart#create'
-  post  '/cart', to: 'cart#update_quantity'
-  delete '/cart', to: 'cart#remove_item'
-  get 'order/delivery', to: 'cart#delivery'
-
-  resources :cart, only: [:index]
+  root 'home#index'
 
 end
