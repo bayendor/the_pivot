83427ca85c7f86a20de4de8aef60e37eb98096a9
Kate Lane <katelane@gmail.com>
2014-12-17 18:32:42 -0700

    User signup form generated. Closes #22.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,6 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def set_items
-    @items = Item.all
-  end
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
