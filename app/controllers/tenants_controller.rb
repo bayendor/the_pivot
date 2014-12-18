e574f2cc337df800e930075954ea48622d955324
Kate Lane <katelane@gmail.com>
2014-12-17 22:18:20 -0700

    added tenant, got display working on the borrowers page

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,9 +1,9 @@
 class TenantsController < ApplicationController
   def index
-    @tenants = Tenant.all 
+    @tenants = Tenant.all
   end
 
   def show
-
+    @tenant = Tenant.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217220716) do
+ActiveRecord::Schema.define(version: 20141218045301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20141217220716) do
     t.string   "slug"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "description"
   end
 
   create_table "user_roles", force: true do |t|
