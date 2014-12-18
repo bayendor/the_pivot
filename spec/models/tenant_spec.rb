8ce91c7f69feffd85f0fab5d82998df92be388b5
Travis Yoder <trayoda@gmail.com>
2014-12-17 22:40:14 -0700

    added tests for tenant spec

--- spec/models/tenant_spec.rb
+++ spec/models/tenant_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Tenant, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Tenant do
+  let(:tenant) do
+    Tenant.create(name: "Mr. Jorge's Place")
+  end
+
+  it "is valid" do
+    expect(tenant).to be_valid
+  end
+
+  it "is valid without a name" do
+    tenant.name = nil
+    expect(tenant).to be_valid
+
+    tenant.name = ""
+    expect(tenant).to be_valid
+  end
+
+  it "can paramterize" do
+    tenant.name = "Jorge goes to the bieber concert"
+    expect(tenant.slug).to eq("mr-jorge-s-place")
+  end
+
+
 end
