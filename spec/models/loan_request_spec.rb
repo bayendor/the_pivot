83986f8df8b8acaa4373a45c667cfb332c16aa46
David Bayendor <dbayendor@gmail.com>
2014-12-18 07:40:36 -0700

    Implements #32 w/ #loan_term, #repayment_rate
    * Includes model tests
    
    * All tests passing

--- spec/models/loan_request_spec.rb
+++ spec/models/loan_request_spec.rb
@@ -1,37 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe LoanRequest do
-
   let(:category) do
-    category = Category.create(name: "name",
-                               description: "description"
+    category = Category.create(name: 'name',
+                               description: 'description'
                               )
   end
 
   let(:category1) do
-    category1 = Category.create(name: "name",
-                                description: "description"
+    category1 = Category.create(name: 'name',
+                                description: 'description'
                                )
-
   end
 
   let(:loan_request) do
-      loan_request = LoanRequest.create(user_id: 1,
-                                        categories: [category, category1],
-                                        title: "title",
-                                        description: "description",
-                                        borrowing_amount: 10,
-                                        amount_funded: 0,
-                                        requested_by_date: DateTime.new,
-                                        payments_begin_date: DateTime.new,
-                                        payments_end_date: DateTime.new,
-                                        status: true
-                                       )
+    loan_request = LoanRequest.create(user_id: 1,
+                                      categories: [category, category1],
+                                      title: 'title',
+                                      description: 'description',
+                                      borrowing_amount: 10_000,
+                                      amount_funded: 0,
+                                      requested_by_date: DateTime.new,
+                                      payments_begin_date: DateTime.now,
+                                      payments_end_date: DateTime.now.months_since(5),
+                                      status: true
+                                     )
   end
 
-  describe "relationships" do
-    it "has many categories" do
+  describe 'relationships' do
+    it 'has many categories' do
       expect(loan_request.categories).to eq([category, category1])
+    end
+  end
+
+  describe 'repayment terms' do
+    it 'calculates the loan term in months' do
+      expect(loan_request.loan_term).to eq(5)
+    end
+
+    it 'calculates the monthly repayment rate' do
+      expect(loan_request.repayment_rate).to eq(2000)
     end
   end
 end
