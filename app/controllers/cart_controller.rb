b084c33358e845b40baf7dbdefa000b1a57d03c4
Travis Yoder <trayoda@gmail.com>
2014-12-18 11:33:02 -0700

    unimplemented the main functionality of the cart controller
    removed the method get_line_item_price
    
    added TODO’s to the methods that will need to be
    re-implemented

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,17 +1,19 @@
 class CartController < ApplicationController
 
+  # TODO: Reimplement this method
   def index
     if cart_has_items
-      @items = Item.order(id: :asc)
-      @cart = session[:cart]
-      @cart_items ||= []
-      get_cart_items
-      get_total_price
+      # @items = Item.order(id: :asc)
+      # @cart = session[:cart]
+      # @cart_loans ||= []
+      # get_cart_items
+      # get_total_price
     else
       redirect_to root_path, notice: "Your cart is empty."
     end
   end
 
+  # TODO: Reimplement this method
   def get_cart_items
     @cart.each do |line_item|
       # cart_line_items = Hash.new
@@ -24,52 +26,51 @@ class CartController < ApplicationController
     # get_line_item_price
   end
 
-  def get_line_item_price
-    @cart_items.each do |line_item|
-      line_item[:price] = line_item[:fillings].map(&:price).reduce(line_item[:item].price, &:+) * line_item[:quantity].to_i
-    end
-  end
-
+  # TODO: Reimplement this method
   def get_total_price
-    @total_price = @cart_items.map { |line_item| line_item[:price] }.reduce(&:+)
+    # @total_price = @cart_items.map { |line_item| line_item[:price] }.reduce(&:+)
   end
 
   def cart_has_items
     session[:cart] != nil && session[:cart] != []
   end
 
+  # TODO: Reimplement this method
   def update_quantity
-    session[:cart].each do |line_item|
-      if line_item["id"] == params[:id]
-        line_item["quantity"] = params[:quantity]
-      end
-    end
+    # session[:cart].each do |line_item|
+    #   if line_item["id"] == params[:id]
+    #     line_item["quantity"] = params[:quantity]
+    #   end
+    # end
     redirect_to cart_index_path, notice: "Item quantity has been updated."
   end
 
+  # TODO: Reimplement this method
   def create
     session[:cart] ||= []
-    if params["item"] != nil
-      line_item = {"item_id" => params["item_id"],
-                   "filling_ids" => params["item"]["filling_ids"],
-                   "quantity" => params["quantity"],
-                   "id" => params["id"]
-                  }
-      session[:cart] << line_item
+    # if params["item"] != nil
+      # line_item = {"item_id" => params["item_id"],
+      #              "filling_ids" => params["item"]["filling_ids"],
+      #              "quantity" => params["quantity"],
+      #              "id" => params["id"]
+      #             }
+      # session[:cart] << line_item
       redirect_to cart_index_path, notice: "Cart created. Please log in."
     else
-      redirect_to :back, notice: "Please add fillings to your item."
+      redirect_to :back, notice: "Please add some loans."
     end
   end
 
+  # TODO: Reimplement this method
   def remove_item
-    session[:cart].delete_if { |line_item| line_item["id"] == params[:id] }
-    redirect_to cart_index_path, notice: "Item quantity has been updated."
+    # session[:cart].delete_if { |line_item| line_item["id"] == params[:id] }
+    # redirect_to cart_index_path, notice: "Item quantity has been updated."
   end
 
   private
 
-    def check_duplicate(line_item)
-       session[:cart].any? {|line| line.include?(line_item["item_id"]) && line.include?(line_item["item"]["filling_ids"]) }
-    end
+  # TODO: Reimplement this method
+  def check_duplicate(line_item)
+    # session[:cart].any? {|line| line.include?(line_item["item_id"]) }
+  end
 end
