f758a8bac85e10bba191854efdf9317dfb21654a
David Bayendor <dbayendor@gmail.com>
2014-12-23 14:16:31 -0700

    Refactor user validations
    * Add errors to view for user#new
    
    * Move validations from view to model
    
    * Cleanup validation test spec
    
    * Closes #95

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,39 +1,44 @@
 class User < ActiveRecord::Base
   belongs_to :tenant
-  has_secure_password
   has_many :loans
   has_many :loan_requests
   has_many :user_roles
   has_many :roles, through: :user_roles
+
+  has_secure_password
+
   validates :first_name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :username, length: {minimum: 2, maximum: 32,
-    too_short: "must have at least %{count} letters",
-    too_long: "must have at most %{count} letters"
-  }
-  validates :password, confirmation: true
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
+  validates :email, presence: true,
+                    format: { with: EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :username, length: { minimum: 2, maximum: 32,
+                                 too_short: 'must have at least %{count} letters',
+                                 too_long: 'must have at most %{count} letters' }
+  validates :password, length: { minimum: 6 }, allow_blank: true
+
   before_validation :assign_username
   before_validation :check_last_name
 
   def is_admin?
-    self.roles.any? { |role| role.name == "admin"}
+    roles.any? { |role| role.name == 'admin' }
   end
 
   def is_a_tenant?
-    self.tenant_id != nil
+    tenant_id != nil
   end
 
   def is_webmaster?
-    self.roles.any? { |role| role.name == "webmaster"}
+    roles.any? { |role| role.name == 'webmaster' }
   end
 
   private
 
   def assign_username
-    self.username ||= self.first_name
+    self.username ||= first_name
   end
 
   def check_last_name
-    self.last_name ||= ""
+    self.last_name ||= ''
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
+RSpec.describe User, type: :model do
   let!(:user) do
-    User.create(first_name: "Jim",
-                last_name: "Jones",
-                email: "jimmy@jones.com",
-                username: "JimJones",
-                password: "password",
-                password_confirmation: "password"
-    )
+    User.create(first_name: 'Jim',
+                last_name: 'Jones',
+                email: 'jimmy@jones.com',
+                username: 'JimJones',
+                password: 'password',
+                password_confirmation: 'password'
+               )
   end
 
-  it "is valid" do
+  it 'is valid' do
     expect(user).to be_valid
   end
 
@@ -19,7 +19,7 @@ RSpec.describe User, :type => :model do
     user.first_name = nil
     expect(user).not_to be_valid
 
-    user.first_name = ""
+    user.first_name = ''
     expect(user).not_to be_valid
   end
 
@@ -29,57 +29,88 @@ RSpec.describe User, :type => :model do
   end
 
   it 'assigns username to first name if username is not present' do
-    user1 = User.create(first_name: "Jim",
-                        last_name: "Jones",
-                        email: "test@jones.com",
-                        password: "password",
-                        password_confirmation: "password"
-    )
-    expect(user1.username).to eq("Jim")
+    user1 = User.create(first_name: 'Jim',
+                        last_name: 'Jones',
+                        email: 'test@jones.com',
+                        password: 'password',
+                        password_confirmation: 'password'
+                       )
+    expect(user1.username).to eq('Jim')
     expect(user1).to be_valid
   end
 
-  it 'can have a username with more than 2 characters but less than 32' do
-    user.username = "j"
-    expect(user).not_to be_valid
+  it 'is invalid if username is too short' do
+    user.username = 'A'
+    expect(user).to_not be_valid
+  end
 
-    user.username = "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
-    expect(user).not_to be_valid
+  it 'is invalid if username is too long' do
+    user.username = ('A' * 33)
+    expect(user).to_not be_valid
   end
 
   it 'cannot create a user with the same email' do
-    user1 = User.create(first_name: "Jim",
-                        last_name: "Jones",
-                        email: "test@jones.com",
-                        username: "Jim",
-                        password: "password",
-                        password_confirmation: "password"
-    )
-    user2 = User.create(first_name: "Jim",
-                        last_name: "Jones",
-                        email: "test@jones.com",
-                        username: "Jim",
-                        password: "password",
-                        password_confirmation: "password"
-    )
+    user1 = User.create(first_name: 'Jim',
+                        last_name: 'Jones',
+                        email: 'test@jones.com',
+                        username: 'Jim',
+                        password: 'password',
+                        password_confirmation: 'password'
+                       )
+    user2 = User.create(first_name: 'Jim',
+                        last_name: 'Jones',
+                        email: 'test@jones.com',
+                        username: 'Jim',
+                        password: 'password',
+                        password_confirmation: 'password'
+                       )
 
     expect(user2).not_to be_valid
   end
 
-  it "assigns first name to username if username isn't given" do
-    user = User.create(first_name: "Jim",
-                        last_name: "Jones",
-                        email: "test@jones.com",
-                        username: nil,
-                        password: "password",
-                        password_confirmation: "password"
-    )
-
-    expect(user.username).to eq("Jim")
+  it 'requires a valid email address' do
+    addresses = %w(user@foo,com user_at_foo.org example.user@foo.
+                   foo@bar_baz.com foo@bar_baz.com)
+    addresses.each do |invalid_address|
+      user.email = invalid_address
+      expect(user).to_not be_valid
+    end
   end
 
-  it "is associated with a role" do
+  it 'is invalid if the email is not unique' do
+    user.save
+    invalid_user = user.dup
+    invalid_user.email = user.email.upcase
+    expect(invalid_user.save).to be false
+  end
+
+  it 'is invalid if password is not present' do
+    user.password = user.password_confirmation = ''
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid if password is too short' do
+    user.password = user.password_confirmation = 'a' * 5
+    expect(user).to_not be_valid
+  end
+  it 'is invalid if password does not match confirmation' do
+    user.password = 'foobar'
+    expect(user).to_not be_valid
+  end
+
+  it "assigns first name to username if username isn't given" do
+    user = User.create(first_name: 'Jim',
+                       last_name: 'Jones',
+                       email: 'test@jones.com',
+                       username: nil,
+                       password: 'password',
+                       password_confirmation: 'password'
+                      )
+
+    expect(user.username).to eq('Jim')
+  end
+
+  it 'is associated with a role' do
     expect(user).to respond_to(:roles)
   end
-
 end
