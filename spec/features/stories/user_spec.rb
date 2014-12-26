74a9694b745b4732ebda2fe86794800fc7417e4d
David Bayendor <dbayendor@gmail.com>
2014-12-26 13:45:15 -0700

    Add test for user profile edit

--- spec/features/stories/user_spec.rb
+++ spec/features/stories/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-describe 'Iteration 1 User Interactions', type: :feature do
-
+describe 'User Interactions', type: :feature do
   context 'a registered user' do
     let(:user) { FactoryGirl.create(:user) }
     before(:each) do
@@ -18,7 +17,33 @@ describe 'Iteration 1 User Interactions', type: :feature do
         expect(page).to have_content('Apply to Become a Borrower!')
         fill_in 'Business Name', with: 'Aperture Labs'
         click_button 'Apply'
-        expect(page).to have_content('Thanks for creating your store') 
+        expect(page).to have_content('Thanks for creating your store')
+        expect(Tenant.count).to eq(tenant_count + 1)
+      end
+    end
+
+    context 'can update their user profile' do
+      it 'has a link to the user profile' do
+        expect(page).to have_link('My Profile')
+      end
+
+      it 'can navigate to an edit profile page' do
+        click_link 'My Profile'
+        expect(page).to have_content('Edit User Profile')
+      end
+
+      it 'can change their username' do
+        click_link 'My Profile'
+        fill_in 'Username', with: 'Cavey'
+        click_button 'Submit'
+        expect(page).to have_content('User was updated.')
+      end
+
+      it 'can change their email' do
+        click_link 'My Profile'
+        fill_in 'Email', with: 'boss@aperturelabs.com'
+        click_button 'Submit'
+        expect(page).to have_content('User was updated.')
       end
     end
   end
