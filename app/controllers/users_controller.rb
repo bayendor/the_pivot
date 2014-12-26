4df17ed4f021b9c245c9fcf2bff61c9d53af6f6f
David Bayendor <dbayendor@gmail.com>
2014-12-26 13:49:19 -0700

    Update users controller
    * Add before filter to set @user
    
    * Add #update
    
    * Refactor for style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   protect_from_forgery with: :null_session
 
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  before_action :current_user, only: [:show, :edit, :update]
+
   def index
     @users = User.all
   end
@@ -9,30 +13,43 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-
   def create
     @user = User.new(user_params)
 
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path, notice: "User created. Please log in."
+      redirect_to root_path, notice: 'User created.'
     else
-      flash.now[:notice] = "User could not be created."
+      flash.now[:notice] = 'User could not be created.'
       render :new
     end
   end
 
-  def destroy
-    @user.destroy
-    redirect_to admin_users_path, notice: "The user was deleted."
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   private
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
-    params.require(:user).permit(:first_name,:last_name,:email, :username, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :username,
+                                 :password, :password_confirmation)
   end
 end
