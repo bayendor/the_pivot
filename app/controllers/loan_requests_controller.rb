c8a80eace00e484f9a4006e5f3feb528d6fdcbd5
David Bayendor <dbayendor@gmail.com>
2014-12-29 13:47:47 -0700

    Refactor loan_request#create
    * Assign user id on creation

--- app/controllers/loan_requests_controller.rb
+++ app/controllers/loan_requests_controller.rb
@@ -1,4 +1,6 @@
 class LoanRequestsController < ApplicationController
+  before_action :set_loan_request, only: [:show, :edit, :update]
+
   def index
     @loan_requests = LoanRequest.order(id: :asc)
     @loan_requests = @loan_requests.status(params[:status]) if params[:status].present?
@@ -10,8 +12,9 @@ class LoanRequestsController < ApplicationController
 
   def create
     @loan_request = LoanRequest.new(loan_request_params)
+    @loan_request.user_id = current_user.id
     if @loan_request.save
-      redirect_to tenant_path
+      redirect_to tenant_path(current_user.tenant.slug), notice: 'Loan Request created.'
     else
       render :new
     end
@@ -24,10 +27,10 @@ class LoanRequestsController < ApplicationController
   private
 
   def loan_request_params
-    params.require(loan_request).permit(:user_id, :title, :description,
-                                        :borrowing_amount, :amount_funded,
-                                        :requested_by_date, 
-                                        :payments_begin_date, 
-                                        :payments_end_date)
+    params.require(:loan_request).permit(:user_id, :title, :description,
+                                         :borrowing_amount, :amount_funded,
+                                         :requested_by_date,
+                                         :payments_begin_date,
+                                         :payments_end_date)
   end
 end
