e33fbb5bc3f821dc5a4a51d07ee5b39afbf06edf
David Bayendor <dbayendor@gmail.com>
2014-12-30 10:06:05 -0700

    Default missing image

--- app/models/loan_request.rb
+++ app/models/loan_request.rb
@@ -7,7 +7,7 @@ class LoanRequest < ActiveRecord::Base
   has_attached_file :image, styles: {
     thumb: '100x100>',
     main: '300x300>'
-  }
+  }, default_url: 'missing.png'
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates :user_id, presence: true
