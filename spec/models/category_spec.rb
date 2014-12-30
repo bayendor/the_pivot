65a1641dfc1ede0a119770ce7e6dd36ccfa76d6e
Kate Lane <katelane@gmail.com>
2014-12-30 14:19:43 -0700

    tests fixed

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -16,8 +16,8 @@ RSpec.describe Category do
   describe "relationships" do
     it "has many loan requests" do
       category = Category.create(name: "name", description: "description")
-      loan_request = LoanRequest.create(user_id: 1, title: "title", description: "description", borrowing_amount: 10, amount_funded: 0, requested_by_date: DateTime.new, payments_begin_date: DateTime.new, payments_end_date: DateTime.new, status: true, categories: [category])
-      loan_request1 = LoanRequest.create(user_id: 2, title: "title1", description: "description1", borrowing_amount: 110, amount_funded: 10, requested_by_date: DateTime.new, payments_begin_date: DateTime.new, payments_end_date: DateTime.new, status: true, categories: [category])
+      loan_request = LoanRequest.create(user_id: 1, title: "title", blurb: "blurb", description: "description", borrowing_amount: 10, amount_funded: 0, requested_by_date: DateTime.new, payments_begin_date: DateTime.new, payments_end_date: DateTime.new, status: true, categories: [category])
+      loan_request1 = LoanRequest.create(user_id: 2, title: "title1", blurb: "blurb", description: "description1", borrowing_amount: 110, amount_funded: 10, requested_by_date: DateTime.new, payments_begin_date: DateTime.new, payments_end_date: DateTime.new, status: true, categories: [category])
       expect(category.loan_requests).to eq([loan_request, loan_request1])
     end
   end
