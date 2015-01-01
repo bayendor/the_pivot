fa537e9b1949563cd582cdba2e23f76fa7b7c918
David Bayendor <dbayendor@gmail.com>
2015-01-01 13:12:43 -0700

    Remove useless routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,28 +3,22 @@ Rails.application.routes.draw do
 
   resources :users
 
-  post   '/cart',          to: 'cart#create'
+  post '/cart',          to: 'cart#create'
   delete '/cart',          to: 'cart#remove_item'
-  post   '/cart_update',   to: 'cart#update_quantity'
+  post '/cart_update',   to: 'cart#update_quantity'
 
   resources :cart, only: [:index]
-  resources :loans
+  resources :loans, only: [:index]
   resources :loan_requests, only: [:index, :new, :show, :create, :update]
 
-  get  '/register', to: 'users#register'
-  post '/register', to: 'users#create'
-
-  get    '/login',  to: 'sessions#new'
-  post   '/login',  to: 'sessions#create'
+  get '/login',  to: 'sessions#new'
+  post '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  get  "tenants", to: "tenants#index"
-  post "tenants", to: "tenants#create"
-
+  get 'tenants', to: 'tenants#index'
+  post 'tenants', to: 'tenants#create'
 
   resources :tenants, as: :tenant, path: '', param: :slug do
-    resources :lists, only: [:show]
-
     get '/admin', to: 'admin/base_admin#index', as: :admin_index
 
     resources :orders, only: [:index, :new, :show, :create]
