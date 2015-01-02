a629824a2d29b8cf81eb14f1804b4db37c86701d
Travis Yoder <trayoda@gmail.com>
2015-01-01 23:44:32 -0700

    Added spaces for readability.
    Changed the crazy xpath find methods to be simple
    click_links. This works because the click_link
    method will also search the alt text of an
    image.
    
    I didn’t add factories to this at the moment because
    it’s doing a larger process of signing up a new user

--- spec/features/checkout_spec.rb
+++ spec/features/checkout_spec.rb
@@ -8,32 +8,32 @@ describe "Checkout" do
       click_link_or_button "Sign Up"
       expect(page).to have_content('Register as a Keevahh User')
 
-      fill_in 'user[first_name]', with: "Chase"
-      fill_in 'user[last_name]', with: "van Hekken"
-      fill_in 'user[email]', with: "chase@gmail.com"
-      fill_in 'user[username]', with: "ChasevH"
-      fill_in 'user[password]', with: "password"
-      fill_in 'user[password_confirmation]', with: "password"
+      fill_in 'user_first_name',            with: "Chase"
+      fill_in 'user_last_name',             with: "van Hekken"
+      fill_in 'user_email',                 with: "chase@gmail.com"
+      fill_in 'user_username',              with: "ChasevH"
+      fill_in 'user_password',              with: "password"
+      fill_in 'user_password_confirmation', with: "password"
       click_button 'Submit'
 
-      find(:xpath, "//a/img[@alt='Borrow']/..").click
+      click_link "Borrow"
       expect(page).to have_content("Apply to Become a Borrower!")
 
-      fill_in 'tenant[name]', with: "Chase Business"
-      fill_in 'tenant[description]', with: "This good description"
+      fill_in 'tenant_name',        with: "Chase Business"
+      fill_in 'tenant_description', with: "This good description"
       click_link_or_button 'Apply'
       expect(page).to have_content("Chase Business")
 
       click_link_or_button "Create new loan request"
       expect(page).to have_content("Create New Loan Request")
 
-      fill_in 'loan_request[title]', with: "I need money"
-      fill_in 'loan_request[blurb]', with: "I need more money"
-      fill_in 'loan_request[description]', with: "I need lots of  money"
-      fill_in 'loan_request[borrowing_amount]', with: "5000"
-      fill_in 'loan_request[requested_by_date]', with: DateTime.new(2015, 01, 01)
-      fill_in 'loan_request[payments_begin_date]', with: DateTime.new(2015, 01, 28)
-      fill_in 'loan_request[payments_end_date]', with: DateTime.new(2015, 12, 28)
+      fill_in 'loan_request_title',               with: "I need money"
+      fill_in 'loan_request_blurb',               with: "I need more money"
+      fill_in 'loan_request_description',         with: "I need lots of  money"
+      fill_in 'loan_request_borrowing_amount',    with: "5000"
+      fill_in 'loan_request_requested_by_date',   with: DateTime.new(2015, 01, 01)
+      fill_in 'loan_request_payments_begin_date', with: DateTime.new(2015, 01, 28)
+      fill_in 'loan_request_payments_end_date',   with: DateTime.new(2015, 12, 28)
       click_link_or_button "Submit"
 
       within("nav") do
@@ -42,8 +42,8 @@ describe "Checkout" do
       expect(page).to have_content("Loan Requests")
 
       find("input[type='checkbox']").set(true)
-      click_link_or_button("Add selected Loans to Cart")
-      click_link_or_button("Checkout")
+      click_link_or_button "Add selected Loans to Cart"
+      click_link_or_button "Checkout"
       expect(page).to have_content("Amount You Funded This Order: $25")
     end
 
@@ -52,15 +52,15 @@ describe "Checkout" do
       click_link_or_button "Sign Up"
       expect(page).to have_content('Register as a Keevahh User')
 
-      fill_in 'user[first_name]', with: "Chase"
-      fill_in 'user[last_name]', with: "van Hekken"
-      fill_in 'user[email]', with: "chase@gmail.com"
-      fill_in 'user[username]', with: "ChasevH"
-      fill_in 'user[password]', with: "password"
-      fill_in 'user[password_confirmation]', with: "password"
+      fill_in 'user_first_name',            with: "Chase"
+      fill_in 'user_last_name',             with: "van Hekken"
+      fill_in 'user_email',                 with: "chase@gmail.com"
+      fill_in 'user_username',              with: "ChasevH"
+      fill_in 'user_password',              with: "password"
+      fill_in 'user_password_confirmation', with: "password"
       click_button 'Submit'
 
-      find(:xpath, "//a/img[@alt='Borrow']/..").click
+      click_link "Borrow"
       expect(page).to have_content("Apply to Become a Borrower!")
 
       fill_in 'tenant[name]', with: "Chase Business"
@@ -71,12 +71,12 @@ describe "Checkout" do
       click_link_or_button "Create new loan request"
       expect(page).to have_content("Create New Loan Request")
 
-      fill_in 'loan_request[blurb]', with: "I need more money"
-      fill_in 'loan_request[description]', with: "I need lots of  money"
-      fill_in 'loan_request[borrowing_amount]', with: "5000"
-      fill_in 'loan_request[requested_by_date]', with: DateTime.new(2015, 01, 01)
-      fill_in 'loan_request[payments_begin_date]', with: DateTime.new(2015, 01, 28)
-      fill_in 'loan_request[payments_end_date]', with: DateTime.new(2015, 12, 28)
+      fill_in 'loan_request_blurb',               with: "I need more money"
+      fill_in 'loan_request_description',         with: "I need lots of  money"
+      fill_in 'loan_request_borrowing_amount',    with: "5000"
+      fill_in 'loan_request_requested_by_date',   with: DateTime.new(2015, 01, 01)
+      fill_in 'loan_request_payments_begin_date', with: DateTime.new(2015, 01, 28)
+      fill_in 'loan_request_payments_end_date',   with: DateTime.new(2015, 12, 28)
       click_link_or_button "Submit"
 
       expect(page).to have_content("Create New Loan Request")
