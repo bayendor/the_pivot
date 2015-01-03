c0649796d6c44b134f2adb384ebb9e54a6dc3c68
Travis Yoder <trayoda@gmail.com>
2015-01-03 14:04:15 -0700

    created a new test that checks that a loan request is closed when it’s funding rate.

---
+++ spec/features/funding_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+describe "Funding" do
+
+  before(:each) do
+    tenant
+    loan_request
+
+    visit root_path
+    fill_in "Username", with: user.username
+    fill_in "Password", with: user.password
+    click_button "Log In"
+
+    visit loan_requests_path
+  end
+
+  let(:loan_request) do
+    FactoryGirl.create(:loan_request_1, user: user)
+  end
+
+  let(:user) do
+    FactoryGirl.create(:user)
+  end
+
+
+  let(:tenant) do
+    FactoryGirl.create(:tenant) do |tenant|
+      tenant.users << user
+    end
+  end
+
+  context "loan_requests" do
+
+    it "has a loan request" do
+      expect(current_path).to eq("/loan_requests")
+      expect(page).to have_content("Borrower: Mr. Jorge")
+    end
+
+    it "is closed when fully funded" do
+      select "475", from: "amount_option_#{loan_request.id}"
+      find(:css, "#loan_requests_[value='#{LoanRequest.first.id}']").set(true)
+      find('input[value="Add selected Loans to Cart"]').click
+      expect(current_path).to eq('/cart')
+
+      click_link "Checkout"
+      expect(page).to have_content("Status: closed")
+    end
+  end
+end
