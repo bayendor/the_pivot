a4846cda39bd53cf31ba0ec0f31db24c33881a64
David Bayendor <dbayendor@gmail.com>
2015-01-03 21:43:03 -0700

    Modify factories and specs for authorization tests

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -1,28 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Loan do
-
   let(:loan) do
     FactoryGirl.create(:loan)
   end
 
-  describe "validations" do
-    it "is valid" do
+  describe 'validations' do
+    it 'is valid' do
       expect(loan).to be_valid
     end
 
-    it "is invalid without a user_id" do
+    it 'is invalid without a user_id' do
       loan.user_id = nil
       expect(loan).to_not be_valid
 
-      loan.user_id = ""
+      loan.user_id = ''
       expect(loan).to_not be_valid
     end
   end
 
-  describe "relationships" do
-    it "has a user" do
-      expect(loan.user.first_name).to eq("Cave")
+  describe 'relationships' do
+    it 'has a user' do
+      expect(loan.user.first_name).to eq('Doug')
     end
   end
 end
