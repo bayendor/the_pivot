60a1549fd303f06a5118c8818737a155ab6b2134
David Bayendor <dbayendor@gmail.com>
2015-01-04 12:20:05 -0700

    Add category ability, refactor to new hash style

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,18 +4,20 @@ class Ability
   def initialize(user)
     user ||= User.new
 
+    can :read, Category
+
     if user.is_a_tenant?
-      can :manage, Tenant, :id => user.tenant_id
+      can :manage, Tenant, id: user.tenant_id
       can :read, LoanRequest
-      can :update, LoanRequest, :user_id => user.id
-      can :create, LoanRequest, :user_id => user.id
-      can :manage, User, :id => user.id
+      can :update, LoanRequest, user_id: user.id
+      can :create, LoanRequest, user_id: user.id
+      can :manage, User, id: user.id
     else
       can :create, Tenant
       can :read, Tenant
       can :read, LoanRequest
       can :update, LoanRequest
-      can :manage, User, :id => user.id
+      can :manage, User, id: user.id
     end
   end
 end
