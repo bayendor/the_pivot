4a7b667de2663bc28acb44548b28429470bebe74
David Bayendor <dbayendor@gmail.com>
2015-01-04 21:21:24 -0700

    Add category to ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,6 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    can :read, Category
 
     if user.is_a_tenant?
       can :manage, Tenant, id: user.tenant_id
@@ -12,12 +11,14 @@ class Ability
       can :update, LoanRequest, user_id: user.id
       can :create, LoanRequest, user_id: user.id
       can :manage, User, id: user.id
+      can :read, Category
     else
       can :create, Tenant
       can :read, Tenant
       can :read, LoanRequest
       can :update, LoanRequest
       can :manage, User, id: user.id
+      can :read, Category
     end
   end
 end
