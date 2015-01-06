60f45fcd457638930e5e94f695fa95e41808a3ee
David Bayendor <dbayendor@gmail.com>
2015-01-05 20:37:54 -0700

    Add controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
+  before_filter :reload_rails_admin, if: :rails_admin_path?
+
   helper_method :current_user, :current_tenant
 
   rescue_from CanCan::AccessDenied do |exception|
@@ -22,4 +24,21 @@ class ApplicationController < ActionController::Base
       redirect_to root_path, notice: 'Webmaster Only'
     end
   end
+
+  private
+
+  def reload_rails_admin
+    models = %W(User UserProfile)
+    models.each do |m|
+      RailsAdmin::Config.reset_model(m)
+    end
+    RailsAdmin::Config::Actions.reset
+
+    load("#{Rails.root}/config/initializers/rails_admin.rb")
+  end
+
+  def rails_admin_path?
+    controller_path =~ /rails_admin/ && Rails.env == 'development'
+  end
+
 end
