21efbf1e6a5a5e46032911652d9a8b677a81ce16
David Bayendor <dbayendor@gmail.com>
2015-01-05 23:48:39 -0700

    Configure for use with cancan authorization

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,7 +1,6 @@
 RailsAdmin.config do |config|
   config.main_app_name = Proc.new { |controller| [ "Keevahh", "Site Administration - #{controller.params[:action].try(:titleize)}" ] }
 
-  config.authorize_with do
-    redirect_to main_app.root_path unless current_user.try(:admin?)
-  end
+  config.authorize_with :cancan
+  config.current_user_method &:current_user
 end
