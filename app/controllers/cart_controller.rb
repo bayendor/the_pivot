b09b3bbd0fb5b455f808a36bd6328b34a400ce66
Travis Yoder <trayoda@gmail.com>
2015-01-06 09:45:43 -0700

    removed logic from the loan_req controller and into the cart controller where it belongs

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -22,6 +22,14 @@ class CartController < ApplicationController
 
   def update
     cart.add_amounts_to_loans(params["amounts"])
+    cart.loans.each do |lr_id, funding|
+      loan_request = LoanRequest.find_by(id: lr_id)
+      loan_request.increment!(:amount_funded, funding.to_i)
+      loan_request.funded! if loan_request.is_funded?
+      Loan.create!(user_id: current_user.id, loan_request_id: lr_id, amount: funding)
+    end
+    session['cart'] = nil
+    flash[:notice] = 'Thanks for your order.'
   end
 
   private
