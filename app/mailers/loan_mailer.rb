09eabc375636786c82d8294edebc4b5bae28a224
ChasevanHekken <cvh1717@gmail.com>
2015-01-06 13:38:47 -0700

    got emails to tenants when receiving funding to work

--- app/mailers/loan_mailer.rb
+++ app/mailers/loan_mailer.rb
@@ -8,16 +8,11 @@ class LoanMailer < ActionMailer::Base
     mail(to: @user.email, subject: "Your order has been processed.")
   end
 
-  def received_money(user, loan_requests, checkout_amounts)
+  def received_money(user, loan_request, checkout_amount)
     @user = user
-    @loans = loan_requests
-    @amounts = checkout_amounts
-    @loans.each do |loan|
-      @recipient = loan.user.email
-      binding.pry
-      mail(to: loan.user.email, subject: "You have received funding.")
-      sleep(10)
-    end
+    @loan = loan_request
+    @amount = checkout_amount
+      mail(to: @loan.user.email, subject: "You have received funding.")
   end
 
 end
