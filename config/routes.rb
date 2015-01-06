5344526afcc08713054953a0569f0f8ae5ec1877
David Bayendor <dbayendor@gmail.com>
2015-01-05 18:25:52 -0700

    Remove unused routes & controllers

--- config/routes.rb
+++ config/routes.rb
@@ -20,17 +20,5 @@ Rails.application.routes.draw do
   get 'tenants', to: 'tenants#index'
   post 'tenants', to: 'tenants#create'
 
-  resources :tenants, as: :tenant, path: '', param: :slug do
-    get '/admin', to: 'admin/base_admin#index', as: :admin_index
-
-    resources :orders, only: [:index, :new, :show, :create]
-
-    namespace :admin do
-      resources :users
-      resources :orders, only: [:index, :show, :update]
-    end
-
-    put 'admin/remove_item', to: 'admin/orders#remove_item'
-    post 'admin/update_quantity', to: 'admin/orders#update_quantity'
-  end
+  resources :tenants, as: :tenant, path: '', param: :slug
 end
