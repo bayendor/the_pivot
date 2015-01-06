d5872f6eb331bce741719c0782e1901c339e25b0
ChasevanHekken <cvh1717@gmail.com>
2015-01-06 14:04:18 -0700

    fix test to use factory girl

--- spec/mailers/loan_mailer_spec.rb
+++ spec/mailers/loan_mailer_spec.rb
@@ -1,53 +1,42 @@
 require "rails_helper"
 
 RSpec.describe LoanMailer, :type => :mailer do
+  before(:each) do
+    loan_request_1
+    loan_request_2
+    tenant
+  end
+
+  let(:loan_request_1) do
+    FactoryGirl.create(:loan_request_1, user: user)
+  end
+
+  let(:loan_request_2) do
+    FactoryGirl.create(:loan_request_2, user: user)
+  end
+
+  let(:user) do
+    FactoryGirl.create(:user)
+  end
+
+  let(:tenant) do
+    FactoryGirl.create(:tenant) do |tenant|
+      tenant.users << user
+    end
+  end
+
   it "sends an email to the user giving money" do
     checkout_loans = []
 
-    current_tenant = Tenant.create!( name: 'My Store',
-                                     description: 'Good Stuff',
-                                    )
-
-    current_user = User.create!(  first_name: 'Bruce',
-                                  last_name:  'Wayne',
-                                  email:      'bruce_wayne@waynewnterprises.com',
-                                  username:   'batman',
-                                  password:   'password',
-                                  tenant_id: current_tenant.id
-                                 )
-
-    loan_request1 = LoanRequest.create!(user_id:             current_user.id,
-                                        title:               "Hello",
-                                        blurb:               'This is a blurb turtles.',
-                                        description:         "nice",
-                                        borrowing_amount:    100,
-                                        amount_funded:       0,
-                                        requested_by_date:   DateTime.now,
-                                        payments_begin_date: DateTime.now.months_since(1),
-                                        payments_end_date:   DateTime.now.months_since(7),
-                                        status:              'open'
-                           )
-    loan_request2 = LoanRequest.create!(user_id:             current_user.id,
-                                        title:               "Goodbye",
-                                        blurb:               'This is a blurb blurb.',
-                                        description:         "bad",
-                                        borrowing_amount:    1000,
-                                        amount_funded:       0,
-                                        requested_by_date:   DateTime.now,
-                                        payments_begin_date: DateTime.now.months_since(1),
-                                        payments_end_date:   DateTime.now.months_since(7),
-                                        status:              'open'
-                           )
-
     checkout_amounts = ["25", "25"]
-    checkout_loans << loan_request1
-    checkout_loans << loan_request2
+    checkout_loans << loan_request_1
+    checkout_loans << loan_request_2
 
-    LoanMailer.lent_money(current_user, checkout_loans, checkout_amounts).deliver
+    LoanMailer.lent_money(user, checkout_loans, checkout_amounts).deliver
     result = ActionMailer::Base.deliveries.last
 
     expect(result).not_to be_nil
-    expect(result.to).to include("bruce_wayne@waynewnterprises.com")
+    expect(result.to).to include("doug@aperturelabs.com")
     expect(result.from).to include("chase@example.com")
     expect(result.subject).to eq("Your order has been processed.")
   end
@@ -55,65 +44,23 @@ RSpec.describe LoanMailer, :type => :mailer do
   it "sends an email to the tenant receiving money" do
     checkout_loans = []
 
-    current_tenant = Tenant.create!( name: 'My Store',
-                                     description: 'Good Stuff',
-                                     blurb: 'blurb'
-                                    )
-    current_user = User.create!(  first_name: 'Bruce',
-                                  last_name:  'Wayne',
-                                  email:      'bruce_wayne@waynewnterprises.com',
-                                  username:   'batman',
-                                  password:   'password',
-                                  tenant_id: current_tenant.id
-                                 )
-
-    current_user2= User.create!(  first_name: 'Tim',
-                                  last_name:  'Billy',
-                                  email:      'tim@gmail.com',
-                                  username:   'superman',
-                                  password:   'password',
-                                 )
-
-    loan_request1 = LoanRequest.create!(user_id:             current_user.id,
-                                        title:               "Hello",
-                                        blurb:               'This is a blurbasdf.',
-                                        description:         "nice",
-                                        borrowing_amount:    100,
-                                        amount_funded:       0,
-                                        requested_by_date:   DateTime.now,
-                                        payments_begin_date: DateTime.now.months_since(1),
-                                        payments_end_date:   DateTime.now.months_since(7),
-                                        status:              'open'
-                           )
-    loan_request2 = LoanRequest.create!(user_id:             current_user.id,
-                                        title:               "Goodbye",
-                                        blurb:               'This is a blurb 123.',
-                                        description:         "bad",
-                                        borrowing_amount:    1000,
-                                        amount_funded:       0,
-                                        requested_by_date:   DateTime.now,
-                                        payments_begin_date: DateTime.now.months_since(1),
-                                        payments_end_date:   DateTime.now.months_since(7),
-                                        status:              'open'
-                           )
-
     checkout_amounts = ["25", "25"]
-    checkout_loans << loan_request1
-    checkout_loans << loan_request2
+    checkout_loans << loan_request_1
+    checkout_loans << loan_request_2
 
     checkout_loans.each_with_index do |loan, index|
-      LoanMailer.received_money(current_user, loan, checkout_amounts[index]).deliver
+      LoanMailer.received_money(user, loan, checkout_amounts[index]).deliver
     end
     result = ActionMailer::Base.deliveries.first
     result2 = ActionMailer::Base.deliveries.last
 
     expect(result).not_to be_nil
-    expect(result.to).to include("bruce_wayne@waynewnterprises.com")
+    expect(result.to).to include("doug@aperturelabs.com")
     expect(result.from).to include("chase@example.com")
     expect(result.subject).to eq("You have received funding.")
 
     expect(result2).not_to be_nil
-    expect(result2.to).to include("bruce_wayne@waynewnterprises.com")
+    expect(result2.to).to include("doug@aperturelabs.com")
     expect(result2.from).to include("chase@example.com")
     expect(result2.subject).to eq("You have received funding.")
   end
