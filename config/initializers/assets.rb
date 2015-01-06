b69b50ed345683290d0f8b059fee8d4e7b7549fe
Travis Yoder <trayoda@gmail.com>
2015-01-06 15:25:32 -0700

    removed other JS calls and added one for the lend file we might use

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,13 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-# Rails.application.config.assets.version = '1.0'
-# Rails.application.config.assets.precompile += %w( main.css )
-# Rails.application.config.assets.precompile += %w( responsive-tables.css )
-# Rails.application.config.assets.precompile += %w( homepage.css )
-# Rails.application.config.assets.precompile += %w( delivery-form.js )
-# Rails.application.config.assets.precompile += %w( hover_description.css )
-# Rails.application.config.assets.precompile += %w( checkbox-handler.js )
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+
+
+# Rails.application.config.assets.precompile += %w( lend.js )
